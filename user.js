24edf93f8048fe48d16200c39a497e062381d318
kk_Ataka <kk_ataka@ring.skr.jp>
2022-02-26 16:56:46 +0900

    Update user.js

--- user.js
+++ user.js
@@ -3,8 +3,8 @@
 // @grant        GM_download
 // @name         PDF to Scrapbox user.js
 // @namespace    http://tampermonkey.net/
-// @version      7.0.0
-// @description  アップロードした画像のurlをテキストで保存
+// @version      8.0.0
+// @description  ドラッグアンドドロップじゃなくてファイル選択方式変更
 // @author       You
 // @match        https://note.gosyujin.com/pdftoscrapbox/*
 // ==/UserScript==
@@ -26,13 +26,107 @@ const GM_save = (title, text) => {
     GM_download(args);
 }
 
+const file = document.querySelector('#getfile');
+const drop = document.querySelector('#drop');
+const filespan = document.querySelector('span.file');
+const page_per = document.querySelector('span.page_per');
+
+// ファイル選択
+(function() {
+    const read = (file)=>{
+        return new Promise((r)=>{
+            const reader = new FileReader();
+            reader.onload = (e)=>{
+                r(e.target.result);
+            };
+            reader.readAsArrayBuffer(file)
+        });
+    }
+
+    const renderAndUpload = async (page, name)=>{
+        const viewport = page.getViewport(1.5);
+        const canvas = document.createElement('canvas') , ctx = canvas.getContext('2d');
+        const renderContext = { canvasContext: ctx, viewport: viewport };
+
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        await page.render(renderContext);
+        const dataUrl = canvas.toDataURL('image/jpeg');
+        const client_id =
+              'a126a3564372324ac926fadea9b4c724f2734dbd734460233a0539b9a84c6ed3';
+
+        const formData = new FormData();
+        formData.append('image_url', dataUrl);
+        formData.append('client_id', client_id);
+        formData.append('referer_url', location.href);
+        formData.append('title', name);
+
+        const response = await fetch('https://upload.gyazo.com/api/upload/easy_auth', { method: 'POST', body: formData });
+        const data = await response.json();
+        const res = await GM_get(data.get_image_url);
+        return res.finalUrl;
+    }
+
+    file.addEventListener('change', async (e) => {
+        console.info(e.srcElement.files[0]);
+        PDFJS.cMapUrl = './cmaps/';
+        PDFJS.cMapPacked = true;
+        const file = e.srcElement.files[0];
+
+        filespan.textContent = '初期化';
+        page_per.textContent = '初期化';
+
+        console.info(file);
+        filespan.textContent = file.name;
+
+        const obj = await read(file);
+        const pdf = await PDFJS.getDocument(obj);
+
+        console.info(pdf.numPages);
+
+        pages = [];
+
+        page = 1;
+        while(true){
+            const i = await pdf.getPage(page);
+
+            let gyazo;
+
+            for (let retry = 1; retry <= 3; retry++) {
+                try {
+                    gyazo = await renderAndUpload(i, file.name);
+                } catch (error) {
+                    console.info(`error and ${retry} retry: ${error}`);
+                }
+                if (gyazo) {
+                    break;
+                }
+            }
+
+            console.info(`${('0000'+page).slice(-4)}: ${gyazo}`);
+            if(pages.includes(gyazo)){
+                console.info(`push skip includes: ${gyazo}`);
+            } else {
+                pages.push(gyazo);
+            }
+            if(page == pdf.numPages) break;
+            page ++;
+            page_per.textContent = `${page} / ${pdf.numPages}`;
+        }
+        const urls = pages.map(url => `[[${url}]]`);
+        urls.unshift(file.name.normalize());
+        window.open(`https://scrapbox.io/${document.querySelector('input').value}/new?body=${encodeURIComponent(urls.join("\n"))}`);
+
+        const plainUrls = pages.map(url => `${url}`);
+        GM_save(file.name.normalize(), plainUrls);
+        console.info(`${file.name.normalize()} done.`);
+    });
+})();
+
+// ドラッグアンドドロップ、書き換えたので未使用
 (function() {
     window.addEventListener('load', ()=>{
-        const drop = document.querySelector('#drop');
-        const filespan = document.querySelector('span.file');
-        const page_per = document.querySelector('span.page_per');
-        const log = document.querySelector('li.log');
-
         const read = (file)=>{
             return new Promise((r)=>{
                 const reader = new FileReader();
@@ -78,15 +172,14 @@ const GM_save = (title, text) => {
 
             filespan.textContent = '初期化';
             page_per.textContent = '初期化';
-            log.textContent = '';
 
-            console.log(file);
+            console.info(file);
             filespan.textContent = file.name;
 
             const obj = await read(file);
             const pdf = await PDFJS.getDocument(obj);
 
-            console.log(pdf.numPages);
+            console.info(pdf.numPages);
 
             pages = [];
 
@@ -100,19 +193,16 @@ const GM_save = (title, text) => {
                     try {
                         gyazo = await renderAndUpload(i, file.name);
                     } catch (error) {
-                        console.log(`error and ${retry} retry: ${error}`);
-                        log.innerHTML += `error and ${retry} retry: ${error}<br />`;
+                        console.info(`error and ${retry} retry: ${error}`);
                     }
                     if (gyazo) {
                         break;
                     }
                 }
 
-                console.log(`${('0000'+page).slice(-4)}: ${gyazo}`);
-                log.innerHTML += `${('0000'+page).slice(-4)}: ${gyazo}<br />`;
+                console.info(`${('0000'+page).slice(-4)}: ${gyazo}`);
                 if(pages.includes(gyazo)){
-                    console.log(`push skip includes: ${gyazo}`);
-                    log.innerHTML += `push skip includes: ${gyazo}<br />`;
+                    console.info(`push skip includes: ${gyazo}`);
                 } else {
                     pages.push(gyazo);
                 }
